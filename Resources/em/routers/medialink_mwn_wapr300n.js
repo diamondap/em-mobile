71d4b2e6af8c9839d83c97061e10bd3c146d734b
A. Diamond <diamond@sheprador.com>
2014-01-01 18:53:40 -0500

    Get list of non-DHCP clients

--- Resources/em/routers/medialink_mwn_wapr300n.js
+++ Resources/em/routers/medialink_mwn_wapr300n.js
@@ -20,6 +20,35 @@ function Medialink_mwn_wapr300n(masterView) {
 	var HttpClient = require('../HttpClient');
 	var client = HttpClient();
 
+	// Traffic stats includes info about all active
+	// clients, including those with static IP addresses.
+	// We don't really care about the stats, but we do
+	// want to know the IP addresses of non-DHCP clients.
+	function getTrafficStats() {
+		var url = baseUrl + "/goform/updateIptAccount";
+		client.get(url, null, function(e) {
+			var lines = client.client.responseText.split("\n");
+			for(var i=0; i < lines.length; i++) {
+				var c = lines[i].replace("'", "").replace("\r", "");
+				var data = c.split(";");
+				var netClient = { 'hostname': 'Unknown Device', 'macAddress': '00:00:00:00:00:00', 'ip4Address': data[0], 'connectionType': data[7] };
+				var entryExists = false;
+				for(var j=0; j < networkClients.length; j++) {
+					if(networkClients[j]['ip4Address'] == netClient['ip4Address']) {
+						Ti.API.info("Entry exists for " + netClient['ip4Address']);
+						entryExists = true;
+						networkClients[j]['connectionType'] = netClient['connectionType'];
+					}
+				}
+				if (entryExists == false) {
+					Ti.API.info("Adding new entry for " + netClient['ip4Address']);
+					networkClients.push(netClient);
+				}				
+			}
+		});		
+	}
+
+	// Most clients are DHCP
 	function getDHCPClientList() {
 		var url = baseUrl + "/lan_dhcp_clients.asp";
 		client.get(url, null, function(e) {
@@ -31,12 +60,13 @@ function Medialink_mwn_wapr300n(masterView) {
 			for(var i=0; i < clientList.length; i++) {
 				var c = clientList[i].replace("'", "");
 				var data = c.split(";");
-				var netClient = { 'hostname': data[0], 'ip4Address': data[1], 'macAddress': data[2] };
+				var netClient = { 'hostname': data[0], 'ip4Address': data[1], 'macAddress': data[2], 'connectionType': 'Wireless' };
 				networkClients.push(netClient);
 				Ti.API.info(data[1]);
 			}
+			getTrafficStats();
 		});
-	}
+	}	
 
 	function getFilterType() {
 		Ti.API.info("getFilterType()");
